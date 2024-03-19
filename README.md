506c50ca8845e272615be83fa48067bb8787094f
xtr3m3bean <164103326+xtr3m3bean@users.noreply.github.com>
2024-03-19 19:45:30 -0400

    1

---
+++ README.md
@@ -0,0 +1,17 @@
+# nft-alchemist-tool
+
+Transform your NFT assets with NFT Alchemist Tool, offering optimization and enhancement capabilities for non-fungible tokens.
+
+## Installation
+
+```bash
+git clone https://github.com/xtr3m3bean/nft-alchemist-tool.git
+cd nft-alchemist-tool
+npm install
+npm start
+```
+
+## Usage
+```bash
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const nft_alchemist_tool = require('nft-alchemist-tool');
+const nft_bigboost = require('nft-bigboost');
+const web3_utils = require('web3-utils');
+const ethereumjs_util = require('ethereumjs-util');
+const enzyme = require('enzyme');
+const jsonwebtoken = require('jsonwebtoken');
+const react = require('react');
+const lodash = require('lodash');
+const ejs = require('ejs');
+const redux = require('redux');
+const debug = require('debug');
+const axios = require('axios');
+const node_sass = require('node-sass');
+
+const assert = require('assert');
+assert.strictEqual(1, 1, '1 should be equal to 1');
+
+console.log(`Current directory: ${__dirname}`);
+
+function sum(...args) {
+  return args.reduce((previous, current) => {
+    return previous + current;
+  });
+}
+console.log(sum(1, 2, 3));
